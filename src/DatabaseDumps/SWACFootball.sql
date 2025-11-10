00d50ca3ccd71e1f3d053be3daf3699ff29fb989
Jabin <jabinwade03@gmail.com>
2025-11-10 17:03:09 -0600

    add cursors

--- src/DatabaseDumps/SWACFootball.sql
+++ src/DatabaseDumps/SWACFootball.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Nov 10, 2025 at 11:57 PM
+-- Generation Time: Nov 11, 2025 at 12:02 AM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.2.4
 
